b0b8fc25dc4de62ff3e861aee609c99bd53c81d6
caio <caiothalles607@gmail.com>
2025-01-18 14:27:25 -0300

    feat: It is possible to count how many appointments and search by month and year

--- app/routes/ScheduleRoutes.py
+++ app/routes/ScheduleRoutes.py
@@ -13,6 +13,12 @@ router = APIRouter(
     tags=["Schedules"],  
 )
 
+@router.get("/total", response_model=int)
+def get_total_schedules(session: Session = Depends(get_session)):
+    total_schedules = session.exec(select(func.count(Schedule.id))).one()
+    return total_schedules
+    
+
 @router.post("/", response_model=Schedule)
 def create_schedule(schedule: Schedule, service_ids: list[int], session: Session = Depends(get_session)):
     
@@ -97,3 +103,37 @@ def update_schedule(schedule_id: int, schedule: Schedule, session: Session = Dep
     session.refresh(db_schedule)
 
     return db_schedule
+
+
+@router.get("/{year}/{month}", response_model=list[ScheduleWithClientPetServices])
+def get_schedules_by_month(
+    year: int,
+    month: int,
+    session: Session = Depends(get_session)
+):
+    try:
+        start_date = datetime(year, month, 1)
+        if month == 12:
+            end_date = datetime(year + 1, 1, 1)
+        else:
+            end_date = datetime(year, month + 1, 1)
+    except ValueError:
+        raise HTTPException(status_code=400, detail="Data inválida. Verifique o ano e o mês informados.")
+
+    statement = (
+        select(Schedule)
+        .where(Schedule.date_schedule >= start_date, Schedule.date_schedule < end_date)
+        .options(
+            joinedload(Schedule.client),
+            joinedload(Schedule.pet),
+            joinedload(Schedule.services)
+        )
+    )
+
+    schedules = session.exec(statement).unique().all()
+
+    if not schedules:
+        raise HTTPException(status_code=404, detail=f"Nenhum agendamento encontrado para {month}/{year}")
+
+    return schedules
+
